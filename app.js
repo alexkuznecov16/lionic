39811470050866812e21ea2befdf140484031120
Alexander Kuznecov <alexander.kuznecov16@gmail.com>
2023-02-28 16:33:12 +0200

    js'

--- app.js
+++ app.js
@@ -3,18 +3,6 @@ btnClose = document.getElementById('hamb-close'),
 hambMenu = document.getElementById('hamb-list'),
 intro = document.getElementById('intro1');
 
-
-// btnAdd.addEventListener('click', () => {
-//     console.log("I am clicked");console.log("I am clicked");
-//     intro.classList.toggle('hamb');
-//     hambMenu.classList.toggle('active');
-//     console.log("I am clicked");
-// })
-
-
-// .intro{
-//     margin-top: 240px;
-// }
 btnAdd.addEventListener('click', () => {
     intro.classList.toggle('hamb');
     hambMenu.classList.toggle('active');
